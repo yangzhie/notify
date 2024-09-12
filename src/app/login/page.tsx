5e1d551e1935759dac96ab0d84e5661b085934f3
ArnavMalasi <arnavmalasi2212@gmail.com>
2024-09-12 13:48:15 +1000

    feature: finished Input components on Login page.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,22 +1,54 @@
+"use client";
+
+import PasswordInput from "@/components/Input/PasswordInput";
 import Navbar from "@/components/Navbar/Navbar";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import { validateEmail } from "@/utils/helper";
 
 function page() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+
+  const handleLogin = async (e: any) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email.");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter a password.");
+      return;
+    }
+
+    setError("");
+  };
   return (
     <>
       <Navbar />
 
       <div className="flex items-center justify-center mt-28">
         <div className="w-96 border rounded bg-white px-7 py-10">
-          <form>
+          <form onSubmit={handleLogin}>
             <h4 className="text-2xl mb-7">Login</h4>
 
             <input
               type="text"
               placeholder="Email"
               className="w-full text-sm bg-transparent border-[1.5px] px-5 py-3 rounded mb-4 outline-none"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
+
+            <PasswordInput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
 
             <button
               type="submit"
---
+++ src/components/Input/PasswordInput.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa'
+
+interface PasswordInputProps {
+    value: string,
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
+    placeholder?: string
+}
+
+function PasswordInput({ value, onChange, placeholder }: PasswordInputProps) {
+    const [isShowPassword, setIsShowPassword] = useState<boolean>(false)
+    const toggleShowPassword = () => {
+        setIsShowPassword(!isShowPassword)
+    }
+  return (
+    <>
+      <div className="flex items-center bg-transparent border-[1.5px] px-5 rounded mb-3">
+        <input
+          onChange={onChange}
+          type={isShowPassword ? "text" : "password"}
+          placeholder={placeholder || "Password"}
+          className="w-full text-sm bg-transparent py-3 mr-3 rounded outline-none"
+        />
+        {isShowPassword ? (
+          <FaRegEye
+            size={22}
+            className="text-primary cursor-pointer"
+            onClick={() => toggleShowPassword()}
+          />
+        ) : (
+          <FaRegEyeSlash
+            size={22}
+            className="text-slate-400 cursor-pointer"
+            onClick={() => toggleShowPassword()}
+          />
+        )}
+      </div>
+    </>
+  );
+}
+
+export default PasswordInput
